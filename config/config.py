47cfc5a3ad47349add62d7fce469a6ee0bd8451d
Jason Handerhan <jmhlax4@gmail.com>
2026-08-02 22:37:12 +0000

    Updates to config.py (added python environment configuration) and added ensemble.py
    Updates to config.py (added python environment configuration) and added ensemble.py. The ensemble.py notebook uses bayesian optimization via optuna to find optimal ensemble blending weights using oof predictions from base model training.

--- config/config.py
+++ config/config.py
@@ -12,3 +12,6 @@ config_loc = '/home/jupyter/Instacart_Reorder_Engine/config'
 
 #Google Cloud Storage Bucket Name
 BUCKET_NAME = "instacart-market-basket-analysis-jhanderhan"
+
+#Python Environment
+PY_ENV = "/opt/micromamba/envs/jupyterlab/bin/python3"
