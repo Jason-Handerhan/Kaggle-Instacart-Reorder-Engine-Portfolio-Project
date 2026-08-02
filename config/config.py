eb77d62819c7f1b8f95c4754c3add8a34b4fda72
Jason Handerhan <jmhlax4@gmail.com>
2026-08-02 21:51:25 +0000

    config.py
    .py file containing file path locations and Cloud Storage Bucket name

---
+++ config/config.py
@@ -0,0 +1,14 @@
+#Root File Location
+source_loc = '/home/jupyter/Instacart_Reorder_Engine'
+
+#Source File Location
+source_loc = '/home/jupyter/Instacart_Reorder_Engine/src'
+
+#Local Outputs Location
+output_loc = '/home/jupyter/Instacart_Reorder_Engine/outputs'
+
+#Local config location (saving optimal weights and hyperparameters)
+config_loc = '/home/jupyter/Instacart_Reorder_Engine/config'
+
+#Google Cloud Storage Bucket Name
+BUCKET_NAME = "instacart-market-basket-analysis-jhanderhan"
